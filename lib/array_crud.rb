a547b5ffa75245fa454882c7643d2a1aa803e4a7
Zaire  Swailes <swaileszaire@gmail.com>
2019-12-16 10:45:38 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+[]
 end
 
 def create_an_array
-  
+["Alex", "Rebecca", "Jennifer", "Tessa"]  
 end
 
 def add_element_to_end_of_array(array, element)
-  
+puts create_an_array<< "Leia"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+puts ("create_an_array", "ana")
 end
 
 def remove_element_from_end_of_array(array)
-  
+puts create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+puts create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+puts create_an_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+puts create_an_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+puts create_an_array[-1]
 end
